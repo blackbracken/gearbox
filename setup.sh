95fd0335f810e9ed985af8dcc4ae0250eca8d5fd
BlackBracken <me@bracken.black>
2019-04-17 20:45:03 +0900

    add jetbrains-toolbox installer

--- setup.sh
+++ setup.sh
@@ -5,6 +5,12 @@ readonly XMODMAP="${HOME}/.xmodmap"
 readonly XPROFILE="${HOME}/.xprofile"
 readonly VIM_CONFIG="${HOME}/.config/nvim/init.vim"
 
+# https://github.com/nagygergo/jetbrains-toolbox-install/blob/master/jetbrains-toolbox.sh
+# licensed under MIT License; Copyright nagygergo 2016
+readonly USER_AGENT='User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'
+readonly URL=$(curl 'https://data.services.jetbrains.com//products/releases?code=TBA&latest=true&type=release' -H 'Origin: https://www.jetbrains.com' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-US,en;q=0.8' -H "${USER_AGENT[@]}" -H 'Accept: application/json, text/javascript, */*; q=0.01' -H 'Referer: https://www.jetbrains.com/toolbox/download/' -H 'Connection: keep-alive' -H 'DNT: 1' --compressed | grep -Po '"linux":.*?[^\\]",' | awk -F ':' '{print $3,":"$4}'| sed 's/[", ]//g')
+readonly JETBRAINS_TOOLBOX="${PWD}/jetbrains-toolbox"
+
 # essential packages
 sudo pacman -S git
 
@@ -25,11 +31,17 @@ echo "export GTK_IM_MODULE=fcitx"    >  $XPROFILE
 echo "export QT_IM_MODULE=fcitx"     >> $XPROFILE
 echo "export XMODIFIERS='@IM=fcitx'" >> $XPROFILE
 
-#rewrite CapsLock to L_Ctrl
+# rewrite CapsLock to L_Ctrl
 echo "keycode  66 = Control_L Control_L Control_L Control_L" >  $XMODMAP
 echo "remove Lock = Control_L"                               >> $XMODMAP
 echo "add Control = Control_L"                               >> $XMODMAP
 echo "/usr/bin/xmodmap ${XMODMAP}"                           >> $BASH
+
+# install jetbrains-toolbox
+wget -c --output-document="${JETBRAINS_TOOLBOX}.tar.gz" $URL
+tar --strip=1 -zxf "${JETBRAINS_TOOLBOX}.tar.gz"
+rm "${JETBRAINS_TOOLBOX}.tar.gz"
+$JETBRAINS_TOOLBOX
 
 # install neovim & dein.vim
 sudo pacman -S neovim
@@ -39,6 +51,9 @@ rm ./dein_installer.sh
 curl https://raw.githubusercontent.com/blackbracken/dotfiles/master/init.vim > $VIM_CONFIG
 sed -i -e "s/blackbracken/${USER}/" $VIM_CONFIG
 echo "alias vim=nvim" >> $BASH
+
+# add util aliases
+echo "alias :q=exit" >> $BASH
 
 # update pacman packages
 sudo pacman -Syu
