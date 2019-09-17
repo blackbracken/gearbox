d517579cfc2f665d508c32f66f8d2cbe7cd497e0
blackbracken <me@bracken.black>
2019-09-18 02:56:58 +0900

    upgrade on executing

--- packages/install.sh
+++ packages/install.sh
@@ -4,6 +4,9 @@ cd `dirname $0`
 yes | sudo pacman --needed -S $(pacman -Qg base-devel | cut -f2 -d " ") \
                                 yay
 
+sudo pacman -Syuu
+yay -Syuu
+
 for language in $(ls ./languages); do
   sh ./languages/$language
 done
