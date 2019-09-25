312f372c5eccd24a9e48cb379f2e3016cb93ffcc
BlackBracken <me@bracken.black>
2019-09-26 01:11:44 +0900

    prevent to reinstall

--- Makefile
+++ Makefile
@@ -1,18 +1,25 @@
 PREFIX := " [.gear]"
-COMMON_PKG := ./init/common
+
+GEAR_ROOT := $$HOME/.gear
+COMMONS_DIR := $(GEAR_ROOT)/init/common
+DOTFILES_DIR := $(GEAR_ROOT)/dotfiles
 
 initialize:
 	@make update
-	for common in $$(ls $(COMMON_PKG)); do \
-		sh "$(COMMON_PKG)/$$common"; \
+	@for common in $$(ls $(COMMONS_DIR)); do \
+		sh "$(COMMONS_DIR)/$$common"; \
 	done
 	@echo "$(PREFIX) All common packages are installed."
 
 update:
+	@git pull origin master
 	@yes | sudo pacman --needed -S yay
 	@yay -Syu --devel --timeupdate
-	@git pull origin master
-	@echo "$(PREFIX) Now .gear is up to date."
+	@for dotfile in $$(ls -A $(DOTFILES_DIR)); do \
+		rm -f $$HOME/$$dotfile; \
+		ln -sf $(DOTFILES_DIR)/$$dotfile $$HOME; \
+	done
+	@echo "$(PREFIX) Now .gear and symbolics are up to date."
 
 help:
 	@echo "$(PREFIX) Usage: make [initialize | update | help]"
--- init/install.sh
+++ init/install.sh
@@ -1,14 +1,26 @@
-#!/bin/bash
+#!/bin/bash -u
 cd `dirname $0`
 
-yes | sudo pacman --needed -S yay
-yay -Syu --devel --timeupdate
+readonly GEAR_ROOT="$HOME/.gear"
 
-for lang in $(ls ./lang); do
-  sh ./lang/$lang
-done
+curl -sL https://raw.githubusercontent.com/blackbracken/.gear/master/res/gear_art | cat
 
-sh ./app.sh
-sh ./japanize.sh
-sh ./neovim.sh
-sh ./powerline.sh
+if [ "`whoami`" == "root" ]; then
+  echo ".gear must be spun without the privilege."
+  exit 1
+elif [ -e $GEAR_ROOT ]; then
+  echo ".gear has already been installed."
+  exit 1
+fi
+
+sudo -v
+
+rm -rf $GEAR_ROOT
+git clone https://github.com/blackbracken/.gear.git $GEAR_ROOT
+
+cd $HOME/.gear
+make initialize
+
+#base64 -d > $HOME/Pictures/wallpaper.jpg < $GEAR_ROOT/res/wallpaper.b64
+#gsettings set org.gnome.desktop.background picture-uri $HOME/Pictures/wallpaper.jpg
+#gsettings set org.gnome.desktop.peripherals.touchpad natural-scroll true
