bef07dfa2cbc615d1b1e7840174f63535502692a
BlackBracken <me@bracken.black>
2019-09-27 18:15:00 +0900

    execute  on initialize

--- init/install.sh
+++ init/install.sh
@@ -19,8 +19,5 @@ rm -rf $GEAR_ROOT
 git clone https://github.com/blackbracken/.gear.git $GEAR_ROOT
 
 cd $HOME/.gear
-make initialize
 
-#base64 -d > $HOME/Pictures/wallpaper.jpg < $GEAR_ROOT/res/wallpaper.b64
-#gsettings set org.gnome.desktop.background picture-uri $HOME/Pictures/wallpaper.jpg
-#gsettings set org.gnome.desktop.peripherals.touchpad natural-scroll true
+make minimal
