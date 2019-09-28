57f0c7ca96664d0b51139921be0946a227613fc7
BlackBracken <me@bracken.black>
2019-09-29 01:38:15 +0900

    add connection editor

--- init/pkg/common/etc.sh
+++ init/pkg/common/etc.sh
@@ -2,6 +2,7 @@
 
 yes | sudo pacman --needed -S powerline-fonts \
                               adobe-source-han-sans-otc-fonts \
+                              nm-connection-editor \
                               exa \
                               tokei
 
