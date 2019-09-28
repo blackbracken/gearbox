8a19d7741222813170638af105ba6d575a4487e1
BlackBracken <me@bracken.black>
2019-09-29 01:51:05 +0900

    resize font on terminal

--- init/pkg/common/etc.sh
+++ init/pkg/common/etc.sh
@@ -3,11 +3,11 @@
 yes | sudo pacman --needed -S powerline-fonts \
                               adobe-source-han-sans-otc-fonts \
                               nm-connection-editor \
+                              gnome-multi-writer \
                               exa \
                               tokei
 
 yay -S powerline-rs \
-       imagewriter \
        gedit \
        jetbrains-toolbox \
        discord \
