16824aee9c8b0c69fac41a7fa56a5d9bd8bbfdea
BlackBracken <me@bracken.black>
2019-09-29 08:14:56 +0900

    change size

--- init/pkg/common/etc.sh
+++ init/pkg/common/etc.sh
@@ -3,6 +3,7 @@
 yes | sudo pacman --needed -S powerline-fonts \
                               adobe-source-han-sans-otc-fonts \
                               nm-connection-editor \
+                              noto-fonts-emoji \
                               gnome-multi-writer \
                               exa \
                               tokei
