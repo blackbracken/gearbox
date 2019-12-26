e5762f4ec217b4a3f9e2adfb2baa6046bedc2b1c
blackbracken <me@bracken.black>
2019-12-27 06:15:18 +0900

    add lightdm

--- init/arch/common/desktop.sh
+++ init/arch/common/desktop.sh
@@ -6,13 +6,16 @@ yay --needed -S xorg-server \
                 lightdm \
                 lightdm-mini-greeter
 
-# change greeter
+# settings for lightdm
 sudo sed -i -e "/greeter-session=/c\greeter-session=lightdm-mini-greeter" /etc/lightdm/lightdm.conf
+sudo sed -i -e "/user-session=/c\user-session=openbox" /etc/lightdm/lightdm.conf
 
-# change user to login as
+# settings for lightdm-mini-greeter
 sudo sed -i -e "/^user = /c\user = $(whoami)" /etc/lightdm/lightdm-mini-greeter.conf
+sudo sed -i -e "/^password-label-text = /c\password-label-text = " /etc/lightdm/lightdm-mini-greeter.conf
+sudo sed -i -e "/^window-color = /c\window-color = \"#1B51B5\"" /etc/lightdm/lightdm-mini-greeter.conf
 
-#rm -rf $HOME/.config/openbox
-#ln -sf $HOME/gearbox/config/openbox $HOME/.config
+rm -rf $HOME/.config/openbox
+ln -sf $HOME/gearbox/config/openbox $HOME/.config/openbox
 
 #sudo systemctl enable lightdm
